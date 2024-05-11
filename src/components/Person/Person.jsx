4f148ff30974b789f52915edef5e73122f8c3fa6
Dmytro Bashlai <imaxs2016@gmail.com>
2024-05-11 18:28:05 +0300

    The component Person has implemented

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let isMarriedMessage = '';
+
+  if (person.isMarried === true && person.sex === 'm') {
+    isMarriedMessage = `${person.partnerName} is my wife`;
+  } else if (person.isMarried === true && person.sex === 'f') {
+    isMarriedMessage = `${person.partnerName} is my husband`;
+  } else {
+    isMarriedMessage = `I am not married`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age !== undefined ? (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      ) : (
+        ''
+      )}
+      <p className="Person__partner">{isMarriedMessage}</p>
+    </section>
+  );
+};
